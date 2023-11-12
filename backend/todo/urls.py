7b39d33f725f5c7266ae67d9c4ab062ede579cc6
akzaylal <99010755+akzaylal@users.noreply.github.com>
2023-11-12 18:47:58 +0530

    Delete backend directory

--- backend/todo/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import TaskListCreateView, TaskRetrieveUpdateDeleteView
-
-urlpatterns = [
-    path('tasks/', TaskListCreateView.as_view(), name='task-list-create'),
-    path('tasks/<int:pk>/', TaskRetrieveUpdateDeleteView.as_view(), name='task-detail'),
-]
--- backend/userprofile/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import UserRegistrationView, UserLoginView, UserLogoutView
-
-urlpatterns = [
-    path('register/', UserRegistrationView.as_view(), name='user-registration'),
-    path('login/', UserLoginView.as_view(), name='user-login'),
-    path('logout/', UserLogoutView.as_view(), name='user-logout'),
-    # Other URL patterns for tasks, etc.
-]
